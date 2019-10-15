e3aab1710f4015534dc882cd90f36a8cc13fbe77
AbdulMajeed <abdul.majeed80254@gmail.com>
2019-10-15 12:39:49 +0530

    Changes

--- student/public/student.js
+++ student/public/student.js
@@ -12,6 +12,8 @@
              var $userNameEdit = $('#userNameEdit')
              var $userAddrEdit = $('#userAddrEdit')
              var $userPhoneEdit = $('#userPhoneEdit')
+
+          // console.log("window.location.href : " + window.location.href);
              $saveuser.submit(function(e){ 
                 e.preventDefault();
                 console.log("here-----")
@@ -24,53 +26,37 @@
                 }
 
                 console.log("is this getting here",$('#userid').val(),$username.val(),$useraddress.val(),$userphone.val())
-                 update()
                 socket.emit('newUser',{id:$('#userid').val(),name:$username.val(),address:$useraddress.val(),phone:$userphone.val()},function(data){
                     
                     if(data){
                     console.log("here",data) 
                     }
+
                 $username.val('');
                 $useraddress.val('');
                 $userphone.val('');
             })
-     
         })
 
-           function update(){
-           var count=parseInt($('#userid').val());
-           $('#userid').val(count+1);
-           console.log($('#userid').val());
-           }
-
-           function update2(){
-           var count=parseInt($('#userEditId').val());
-           $('#userEditId').val(count+1);
-           console.log($('#userEditId').val());
-           }
-
               socket.on('saveUser',function(data){
-               console.log("user getting saved",data)
+               console.log("user getting saved",data) 
               for(var i=0;i<data.length;i++) {       
-               $(".data-table tbody").append("<tr><td>"+data[i].name+"</td><td>"+data[i].address+"</td><td>"+data[i].phone+"</td><td><a href='/update/"+data[i].id+"' class='btn btn-info btn-xs btn-edit'>Edit</a><a href='/delete/"+data[i].id+"' class='btn btn-danger btn-xs btn-delete'>Delete</a></td></tr>");
-              }           
+               $(".data-table tbody").append("<tr><td>"+data[i].name+"</td><td>"+data[i].address+"</td><td>"+data[i].phone+"</td><td><a href='/update/"+data[i].id+"' class='btn btn-info btn-xs btn-edit'>Edit</a><a href='/delete/"+data[i].id+"' class='btn btn-danger btn-xs btn-delete'>Delete</a></td></tr>");       
+          //   console.log(window.location.pathname,"update/"+data[i].id)          
+              }  
             })
 
-            socket.on("data",function(data){
-                console.log(data,"data from the submit tab")
-                $('#userEditId').on('click',function(e){
-                $('#userEditId').trigger('myEvent', {id: data.id})
-               });
-                console.log("here")
-            })
+
+   $("body").on("click", ".btn-edit", function(){
+     console.log("is it coming here")
+     console.log(window.location.pathname)
+    });
+
 
           $editDetails.submit(function(e){
             e.preventDefault();
             console.log("in edit details");
-
-            $('#userEditId').on('myEvent',function(e,data){
-            console.log(data.id);
-             });
+            console.log(window.location.pathname,"update")
 
           if($userNameEdit.val()=='' || $userNameEdit.val()==" "||$userAddrEdit.val()=='' || $userAddrEdit.val()==" "
                     || $userPhoneEdit.val()=='' || $userPhoneEdit.val()==" ")
@@ -80,7 +66,6 @@
                 }
 
                 console.log("is this getting here too",$('#userEditId').val(),$userNameEdit.val(),$userAddrEdit.val(),$userPhoneEdit.val())
-                update2()
                 socket.emit('editUser',{id:$('#userEditId').val(),name:$userNameEdit.val(),address:$userAddrEdit.val(),phone:$userPhoneEdit.val()},function(data){        
                 if(data){
                 console.log("here") 
@@ -90,13 +75,12 @@
                 $userPhoneEdit.val('');
             });
         })
-        
+
         socket.on('editedUser',function(data){
          console.log("edited user values")
         $(".data-table tbody").append("<tr><td>"+data.name+"</td><td>"+data.address+"</td><td>"+data.phone+"</td><td><a href='/update/"+data.id+"' class='btn btn-info btn-xs btn-edit'>Edit</a><a href='/delete/"+data.id+"' class='btn btn-danger btn-xs btn-delete'>Delete</a></td></tr>");        
           })
          
-
       $(window).on('load', function(){   
       socket.emit('oldUser',{id:$('#userEditId').val(),name:$username.val(),address:$useraddress.val(),phone:$userphone.val()})   
       })   
